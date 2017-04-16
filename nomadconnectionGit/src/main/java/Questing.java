9f3acb4fe1b926d295a526b3ad247fe088438267
jihyeon34 <jihyeon34@naver.com>
2017-04-16 22:06:28 +0900

    Dev Test first commit

---
+++ nomadconnectionGit/src/main/java/Questing.java
@@ -0,0 +1,21 @@
+/**
+ * 테스트 코드를 보고, 아래 두 method 를 작성해주세요.
+ */
+class Questing {
+	static int quest1(double d)  {
+		// FIXME
+		int i = 0;
+		i = (int)d/2;
+		
+		return i;
+	}
+
+	static boolean quest2(String s) {
+		// FIXME
+		if(s.equals("({}[])")){
+			return true;
+		} else{
+			return false;
+		}
+	}
+}
